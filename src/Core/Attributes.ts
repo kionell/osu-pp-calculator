0e05a81b13bd1e39317714ec8cc64503cf50fb53
kionell <sashakostyunin1@gmail.com>
2022-05-17 07:46:50 +0300

    Move difficulty mods filter to utils

--- src/Core/Attributes.ts
+++ src/Core/Attributes.ts
@@ -1,15 +1,12 @@
-import {
-  Beatmap,
-  type DifficultyAttributes,
-  type PerformanceAttributes,
-  type ModCombination,
+import type {
+  DifficultyAttributes,
+  PerformanceAttributes,
 } from 'osu-classes';
 
 import type {
   IDifficultyCalculationOptions,
   IPerformanceCalculationOptions,
 } from './Interfaces';
-import { getRulesetById } from './Utils';
 
 /**
  * Calculates difficulty attributes by ID, custom file or IBeatmap object.
@@ -49,30 +46,4 @@ export function calculatePerformance(options: IPerformanceCalculationOptions): P
   const calculator = ruleset.createPerformanceCalculator(difficulty, scoreInfo);
 
   return calculator.calculateAttributes();
-}
-
-/**
- * Filters mods from combination to get only difficulty mods.
- * @param ruleset Target ruleset ID.
- * @param mods Original mods.
- * @returns Difficulty mods.
- */
-export function getDifficultyMods(rulesetId: number, mods: string | number): ModCombination {
-  const ruleset = getRulesetById(rulesetId);
-  const difficultyCalculator = ruleset.createDifficultyCalculator(new Beatmap());
-  const difficultyMods = difficultyCalculator.difficultyMods;
-  const combination = ruleset.createModCombination(mods);
-
-  const difficultyBitwise = combination.all.reduce((bitwise, mod) => {
-    const found = difficultyMods.find((m) => {
-      if (m.bitwise === mod.bitwise) return true;
-
-      // Special case for DT/NC.
-      return m.acronym === 'DT' && mod.acronym === 'NC';
-    });
-
-    return bitwise + (found?.bitwise ?? 0);
-  }, 0);
-
-  return ruleset.createModCombination(difficultyBitwise);
 }
