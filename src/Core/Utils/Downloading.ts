ab23b53906332b0161901ce6dc6dc634496dc749
Alexander <sashakostyunin1@gmail.com>
2022-11-01 13:40:09 +0200

    Prevent 429 error on bulk calculations

--- src/Core/Utils/Downloading.ts
+++ src/Core/Utils/Downloading.ts
@@ -5,6 +5,9 @@ import {
   IDownloadEntryOptions,
 } from 'osu-downloader';
 
+// Create a global downloader instance to prevent 429 error.
+const downloader = new Downloader();
+
 /**
  * Downloads an osu! file by ID or URL.
  * @param path Path to the file save location.
@@ -12,7 +15,10 @@ import {
  * @returns Download result.
  */
 export async function downloadFile(path?: string, options?: IDownloadEntryOptions): Promise<DownloadResult> {
-  const downloader = new Downloader({ rootPath: path });
+  if (downloader.rootPath !== path) {
+    downloader.updateSettings({ rootPath: path });
+  }
+
   const entry = new DownloadEntry(options);
 
   downloader.addSingleEntry(entry);
